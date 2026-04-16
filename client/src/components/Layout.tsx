362d1f35f335eeaf096772d1fe47091123932a39
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-04-16 21:33:28 +0300

    feat(layout): wrap Outlet with constrained main container

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -7,7 +7,9 @@ export const Layout = () => {
 	return (
 		<div className='flex flex-col min-h-screen pt-14'>
 			<Header />
-			<Outlet />
+			<main className='w-full max-w-300 mx-auto px-4 mb-10'>
+				<Outlet />
+			</main>
 			<Footer />
 		</div>
 	)
