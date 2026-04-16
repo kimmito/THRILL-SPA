fcbd12502c3dac6b0ba99e2e954c7da5929bde77
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-04-16 21:34:08 +0300

    feat(home): refactor hero, pull services into array and include Map

--- client/src/screens/home/Home.tsx
+++ client/src/screens/home/Home.tsx
@@ -1,90 +1,76 @@
 import { Button } from 'antd'
 
+import Map from '@/components/Map'
+
 import hallImage from '@/assets/images/hall.jpg'
 
 const Home = () => {
+	const services = [
+		{ name: 'Маникюр', link: '#' },
+		{ name: 'Педикюр', link: '#' },
+		{ name: 'Брови', link: '#' },
+		{ name: 'Ресницы', link: '#' }
+	]
 	return (
-		<main className='flex'>
-			<div className='flex-1 mx-20 text-center items-center justify-center flex flex-col'>
-				<nav className='w-full'>
-					<ul className='flex justify-center gap-10 mb-10 w-full'>
-						<li>
-							<Button
-								type='text'
-								variant='outlined'
-								color='purple'
-								ghost={true}
-							>
-								О нас
-							</Button>
-						</li>
-						<li>
-							<Button
-								type='text'
-								variant='outlined'
-								color='purple'
-								ghost={true}
-							>
-								Услуги
-							</Button>
-						</li>
-						<li>
-							<Button
-								type='text'
-								variant='outlined'
-								color='purple'
-								ghost={true}
-							>
-								Прайс
-							</Button>
-						</li>
-						<li>
-							<Button
-								type='text'
-								variant='outlined'
-								color='purple'
-								ghost={true}
-							>
-								Контакты
-							</Button>
-						</li>
+		<div>
+			<div className='flex'>
+				<div className='flex flex-1 mx-20 text-center items-center justify-center flex-col '>
+					<nav className='w-full'>
+						<ul className='flex justify-center gap-10 mb-10 w-full'>
+							{services.map(service => (
+								<li key={service.name}>
+									<Button
+										type='text'
+										variant='outlined'
+										color='purple'
+										ghost={true}
+									>
+										{service.name}
+									</Button>
+								</li>
+							))}
+						</ul>
+					</nav>
+					<h2 className='text-2xl'>Салон красоты в Краснодаре</h2>
+					<h1 className='text-6xl font-title font-semibold uppercase'>
+						Thrill
+					</h1>
+					<ul className='flex mt-6 justify-center text-lg [&>li]:after:content-["|"] [&>li]:after:mx-4 [&>li]:last:after:content-[""]'>
+						{services.map(service => (
+							<li key={service.name}>
+								<span className='lowercase'>{service.name}</span>
+							</li>
+						))}
 					</ul>
-				</nav>
-				<h2 className='text-2xl'>Салон красоты в Краснодаре</h2>
-				<h1 className='text-6xl font-title font-semibold uppercase'>Thrill</h1>
-				<ul className='flex mt-6 justify-center text-lg [&>li]:after:content-["|"] [&>li]:after:mx-4 [&>li]:last:after:content-[""]'>
-					<li>маникюр</li>
-					<li>педикюр</li>
-					<li>брови</li>
-					<li>ресницы</li>
-				</ul>
-				<div className='mt-12 flex gap-10 justify-center'>
-					<Button
-						variant='solid'
-						color='purple'
-						size='large'
-						style={{ width: '200px' }}
-					>
-						Онлайн-запись
-					</Button>
-					<Button
-						variant='solid'
-						color='purple'
-						size='large'
-						style={{ width: '200px' }}
-					>
-						Прайс
-					</Button>
+					<div className='mt-12 flex gap-10 justify-center'>
+						<Button
+							variant='solid'
+							color='purple'
+							size='large'
+							style={{ width: '200px' }}
+						>
+							Онлайн-запись
+						</Button>
+						<Button
+							variant='solid'
+							color='purple'
+							size='large'
+							style={{ width: '200px' }}
+						>
+							Прайс
+						</Button>
+					</div>
+				</div>
+				<div className='flex-1 '>
+					<img
+						src={hallImage}
+						alt='Салон красоты THRILL'
+						className='h-[90vh] relative right-0 -top-14 shadow-lg shadow-fuchsia-950/20 rounded-b-lg overflow-hidden'
+					/>
 				</div>
 			</div>
-			<div className='flex-1'>
-				<img
-					src={hallImage}
-					alt='Салон красоты Euphoria SPA'
-					className='h-[90vh] relative right-0 -top-14'
-				/>
-			</div>
-		</main>
+			<Map />
+		</div>
 	)
 }
 
