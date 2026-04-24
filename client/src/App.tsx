2d83ad3fa4f4a64474158ed0cc8ad2ab0bcb5ca7
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-04-24 22:33:17 +0300

    feat(theme): configure shared Tailwind and Ant Design theme tokens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,6 @@ import { AppRouter } from '@navigation/AppRouter'
 import { appRoutes } from '@navigation/routes.config'
 import { useRouteTitle } from '@navigation/useRouteTitle'
 
-import './global.css'
-
 function App() {
 	useRouteTitle(appRoutes)
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
-import 'antd/dist/antd.css'
+import './global.css'
 import { StrictMode } from 'react'
+import { ConfigProvider } from 'antd'
+import { StyleProvider } from '@ant-design/cssinjs'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router'
@@ -9,10 +11,32 @@ import { store } from './store/store'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
+		<StyleProvider layer>
+			<ConfigProvider
+				theme={{
+					token: {
+						colorPrimary: '#3B3658',
+						colorBorder: '#786A99',
+						colorText: '#CCCCCC',
+						colorTextBase: '#CCCCCC',
+						borderRadius: 0,
+						fontFamily: "'Arsenal SC', sans-serif"
+					},
+					components: {
+						Button: {
+							borderRadius: 0,
+							primaryShadow: 'none',
+							defaultShadow: 'none'
+						}
+					}
+				}}
+			>
+				<Provider store={store}>
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+				</Provider>
+			</ConfigProvider>
+		</StyleProvider>
 	</StrictMode>
 )
