6e3f6b48d702b8966bf6b3af818ffc0cbd5cc347
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-04-26 00:25:12 +0300

    feat(layout): add animated gradient background

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,19 +1,45 @@
 import { Outlet } from 'react-router'
 
 import Footer from './Footer'
+import Grainient from './Gradient'
 import Header from './Header'
-import { ServicesRefProvider } from '@/contexts/ServicesRefProvider'
 
 export const Layout = () => {
 	return (
-		<ServicesRefProvider>
-			<div className='flex flex-col min-h-screen pt-14'>
+		<div className='layout relative'>
+			<div className='flex flex-col min-h-screen relative '>
 				<Header />
-				<main className='w-full max-w-350 mx-auto px-4 mb-10'>
+				<main className='w-full mx-auto mb-10 relative'>
+					<div className='absolute top-0 left-0 w-full h-full -z-100000'>
+						<Grainient
+							color1='#201232'
+							color2='#191526'
+							color3='#291737'
+							timeSpeed={2}
+							colorBalance={0.26}
+							warpStrength={1.05}
+							warpFrequency={4.1}
+							warpSpeed={2}
+							warpAmplitude={50}
+							blendAngle={32}
+							blendSoftness={0.9}
+							rotationAmount={810}
+							noiseScale={0.8}
+							grainAmount={0}
+							grainScale={0.3}
+							grainAnimated={false}
+							contrast={1}
+							gamma={1}
+							saturation={1}
+							centerX={-0.2}
+							centerY={0}
+							zoom={0.3}
+						/>
+					</div>
 					<Outlet />
 				</main>
 				<Footer />
 			</div>
-		</ServicesRefProvider>
+		</div>
 	)
 }
