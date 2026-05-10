3bbd2d09dcaa2eff1bf63108197c09ed89f5e0c9
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-05-11 01:00:02 +0300

    style(map): refine contact block layout

--- client/src/pages/home/MapSection.tsx
+++ client/src/pages/home/MapSection.tsx
@@ -3,7 +3,6 @@ import { FaCalendarCheck, FaPhoneAlt } from 'react-icons/fa'
 import { IoMdMailOpen } from 'react-icons/io'
 
 import mapPreview from '@/assets/images/map-preview.png'
-import womanImage from '@/assets/images/woman.png'
 
 const MapSection = () => {
 	const [isMapLoaded, setIsMapLoaded] = useState(false)
@@ -13,39 +12,33 @@ const MapSection = () => {
 			className='flex flex-col max-w-370 mx-auto shadow-lg shadow-black/20 lg:flex-row mt-20 mb-30'
 			id='map'
 		>
-			<div className='relative flex w-full flex-col pt-10 justify-start gap-1 bg-panel p-10 pl-8 text-copy lg:w-1/2 lg:pl-20'>
-				<h3 className='mb-3 text-2xl font-bold'>Краеведа Соловьёва</h3>
-				<p className='mb-10 text-sm text-copy/60'>
+			<div className='relative flex w-full flex-col pt-16 justify-start gap-1 bg-panel p-10 pl-8 text-copy lg:w-1/2 lg:pl-20'>
+				<h3 className='mb-2 text-4xl font-bold'>Краеведа Соловьёва</h3>
+				<p className='mb-4 text-md text-copy/60'>
 					Краснодар, ул. Краеведа Соловьёва 6, к. 3
 				</p>
-				<p className='text-xl font-bold'>
+				<p className='text-2xl font-bold hover:text-accent'>
 					<FaPhoneAlt className='mr-2 inline' size={14} />
 					<a href='tel:+79615177332'>+7 (961) 517-73-32</a>
 				</p>
-				<p className='text-sm'>
-					<IoMdMailOpen className='relative bottom-0.5 mr-2 inline' size={17} />
+				<p className='text-[19px] hover:text-accent'>
+					<IoMdMailOpen className='group mr-1.5 inline' size={17} />
 					<a
 						href='mailto:thrill@gmail.com'
-						className='text-copy/80 hover:text-accent'
+						className='text-copy/80 '
 					>
 						thrill@gmail.com
 					</a>
 				</p>
-				<p className='text-sm'>
+				<p className='text-[16px]'>
 					<FaCalendarCheck
 						className='relative bottom-0.5 mr-1.5 inline'
 						size={17}
 					/>
 					Пн-Вс: 10:00-22:00
 				</p>
-				<img
-					src={womanImage}
-					alt=''
-					aria-hidden='true'
-					className='absolute pointer-events-none z-0 mx-auto h-80 w-[90%] -bottom-20 -left-15 mix-blend-multiply'
-				/>
 			</div>
-			<div className='relative h-120! w-full overflow-hidden bg-panel lg:h-auto lg:w-2/3'>
+			<div className='relative h-100! w-full overflow-hidden bg-panel lg:h-auto lg:w-2/3'>
 				<div className='absolute inset-0'>
 					<img
 						src={mapPreview}
