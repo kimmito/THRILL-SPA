f95d0beca07e57dfcf5e17074cea13a8cf6063ed
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-08-07 23:02:04 +0300

    feat(feedback): disabled scrolling ability when modal is open

--- client/src/features/feedback/FeedbackWidget.tsx
+++ client/src/features/feedback/FeedbackWidget.tsx
@@ -14,6 +14,24 @@ export const FeedbackWidget = () => {
 	}
 
 	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = 'hidden'
+			document.body.style.position = 'fixed'
+			document.body.style.width = '100%'
+		} else {
+			document.body.style.overflow = ''
+			document.body.style.position = ''
+			document.body.style.width = ''
+		}
+
+		return () => {
+			document.body.style.overflow = ''
+			document.body.style.position = ''
+			document.body.style.width = ''
+		}
+	}, [isOpen])
+
+	useEffect(() => {
 		const alreadyShown = sessionStorage.getItem('feedback-modal-shown')
 		if (alreadyShown) return
 
