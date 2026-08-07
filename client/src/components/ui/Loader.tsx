c1ae83ae8870c95aee3e6c7d54534f35a47efc6f
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-08-07 23:00:12 +0300

    style(loader): switch loader position to center

--- client/src/components/ui/Loader.tsx
+++ client/src/components/ui/Loader.tsx
@@ -2,16 +2,18 @@ import { MutatingDots } from 'react-loader-spinner'
 
 export const Loader = () => {
 	return (
-		<MutatingDots
-			visible={true}
-			height='100'
-			width='100'
-			color='#ffe476'
-			secondaryColor='#ffe476'
-			radius='12.5'
-			ariaLabel='mutating-dots-loading'
-			wrapperStyle={{}}
-			wrapperClass=''
-		/>
+		<div className='flex justify-center items-center h-[400px]'>
+			<MutatingDots
+				visible={true}
+				height='100'
+				width='100'
+				color='#ffe476'
+				secondaryColor='#ffe476'
+				radius='12.5'
+				ariaLabel='mutating-dots-loading'
+				wrapperStyle={{}}
+				wrapperClass=''
+			/>
+		</div>
 	)
 }
