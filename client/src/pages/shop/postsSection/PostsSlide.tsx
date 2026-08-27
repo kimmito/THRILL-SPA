bbadddc6d79d90658e779d59ed91c3537143e8c5
Egor Danilov <159701362+kimmito@users.noreply.github.com>
2026-08-27 15:33:22 +0300

    feat(posts): add posts images

--- client/src/pages/shop/postsSection/PostsSlide.tsx
+++ client/src/pages/shop/postsSection/PostsSlide.tsx
@@ -7,12 +7,12 @@ interface PostsSlideProps {
 export const PostsSlide = ({ post }: PostsSlideProps) => {
 	return (
 		<div className='flex flex-row justify-center items-center gap-4 mx-auto select-none mt-5'>
-
-			<div className='bg-[#5F5F5F] pl-30 pt-7 pb-10 min-w-[700px] bg-cover bg-center bg-no-repeat' >
+			<div
+				className={`bg-[#5F5F5F] bg-[url(${post.imagePath})] pl-30 pt-7 pb-10 min-w-[700px] bg-cover bg-center bg-no-repeat`}
+			>
 				<h3 className='text-[44px] text-head'>{post.title}</h3>
 				<p className='text-2xl text-head'>{post.description}</p>
 			</div>
-
 		</div>
 	)
 }
